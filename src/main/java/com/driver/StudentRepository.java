01fc7bf3428ff1f54278f78128ed6f3ec5069b26
Wajid <ansariwajid9999@gmail.com>
2023-08-17 23:28:56 +0530

    aniket code reference

--- src/main/java/com/driver/StudentRepository.java
+++ src/main/java/com/driver/StudentRepository.java
@@ -1,56 +1,41 @@
 package com.driver;
 
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Repository;
 
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.stream.Collectors;
 
 @Repository
-public class StudentRepository {
-    HashMap<String,Student> studentHashMap = new HashMap<>();
-    HashMap<String,Teacher> teacherHashMap = new HashMap<>();
-    HashMap<String, List<String>> studentTeacherHashMap = new HashMap<>();
-    public void addStudent(Student student){
-        studentHashMap.put(student.getName(),student);
+public class StudentRepository
+{
+    private Map<String, Student> studentMap=new HashMap<>();
+    private Map<String, Teacher>teacherMap=new HashMap<>();
+    private Map<String, List<Student>> teacherStudent=new HashMap<>();
+
+    public Map<String, Student> getStudentMap()
+    {
+        return studentMap;
     }
-    public void addTeacher(Teacher teacher){
-        teacherHashMap.put(teacher.getName(),teacher);
+
+    public Map<String, Teacher> getTeacherMap()
+    {
+        return teacherMap;
     }
-    public void addStudentTeacherPair(String student, String teacher){
-        List<String> StudentList = studentTeacherHashMap.getOrDefault(teacher,new ArrayList<>());
-        StudentList.add(student);
+
+    public Map<String, List<Student>> getTeacherStudent() {
+        return teacherStudent;
     }
-    public Student getStudentByName(String name){
-        Student student = studentHashMap.getOrDefault(name,null);
-        return student;
+
+    public void setStudentMap(Map<String, Student> studentMap) {
+        this.studentMap = studentMap;
     }
-    public Teacher getTeacherByName(String name){
-        Teacher teacher = teacherHashMap.getOrDefault(name,null);
-        return teacher;
+
+    public void setTeacherMap(Map<String, Teacher> teacherMap) {
+        this.teacherMap = teacherMap;
     }
-    public List<String> getStudentsByTeacherName(String teacher){
-        return studentTeacherHashMap.getOrDefault(teacher,new ArrayList<>());
-    }
-    public List<String> getAllStudents(){
-        List<List<String>> AllStudents = new ArrayList<>();
-        for(Map.Entry<String,List<String>> entry : studentTeacherHashMap.entrySet()){
-            AllStudents.add(entry.getValue());
-        }
-        List<String> allStudents = AllStudents.stream()
-                .flatMap(List::stream)
-                .collect(Collectors.toList());
-        return allStudents;
-    }
-    public void deleteTeacherByName(String teacher){
-        teacherHashMap.remove(teacher);
-    }
-    public void deleteAllTeachers(){
-        for(Map.Entry<String,Teacher> entry : teacherHashMap.entrySet()){
-            teacherHashMap.remove(entry.getKey());
-        }
+
+    public void setTeacherStudent(Map<String, List<Student>> teacherStudent) {
+        this.teacherStudent = teacherStudent;
     }
 }
